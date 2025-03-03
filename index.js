a5708479ed789cd488e39bd7e1374683d3ef06f2
koushikrdy <koushikrdyt@gmail.com>
2025-03-03 16:50:38 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(cors({ origin: 'http://localhost:3001', methods: ['GET', 'POST'], allowedHeaders: ['Content-Type'] }));
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmToCustomer', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/products', require('./routes/products'));
+app.use('/api/orders', require('./routes/orders'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
